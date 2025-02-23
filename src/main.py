f43539b1af95a56044653994d436fa9f380bf23a
David <david.munoz19@alu.uclm.es>
2025-02-23 11:39:13 +0100

    Formateo

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 import os
 import pandas as pd
+import re
 
+def clean_location(text):
+    if pd.isna(text):
+        return text
+    text = re.sub(r"\.{3}$", "", text) # Elimina los puntos suspensivos al final del texto
+    text = re.sub(r"^\s*(near|whitin)\s+", "", text) # Elimina "near" y "whitin" al principio del texto
+    return text.strip() # Elimina los espacios en blanco al principio y al final del texto
 
-
-def load(dataset):
-    df = pd.read_csv(dataset, header=0, delimiter=",")
-
+def transform(df):
     # Las fechas que aparecen incompletas las formatea como 01-JAN-YYYY
     df['Incident_Date'] = df['Incident_Date'].apply(lambda x: x.replace('??', '01').replace('???', 'JAN'))
 
@@ -13,11 +17,24 @@ def load(dataset):
     df = df.drop_duplicates()
 
     # Limpia los datos de la columna "Time" a únicamente la hora
-    df["Time"] = df["Time"].astype(str).str.extract(r'(\d{1,2}:\d{2})')
+    df["Time"] = df["Time"].astype(str).str.extract(r'(\d{2}:\d{2})')
+
+    # Limpia los datos de la columna "Aircraft_First_Flight" a únicamente el año
+    df["Aircaft_First_Flight"] = df["Aircaft_First_Flight"].astype(str).str.extract(r"(\d{4})")
 
     # Sutituye los valores nulos por "-"
     df.fillna("-", inplace=True)
 
+    print(df["Aircaft_First_Flight"])
+    
+
+def extract(dataset):
+    df = pd.read_csv(dataset, header=0, delimiter=",")
+
+    return df
+
+
+def load(df):
 
 
    # print(df.dtypes)
@@ -32,7 +49,8 @@ def load(dataset):
             
 def main():
     dataset = "../data/Aircraft_Incident_Dataset.csv"
-    load(dataset)
+    df = extract(dataset)
+    df = transform(df)
 
 if __name__ == "__main__":
     main()
