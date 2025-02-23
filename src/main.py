35e866696b921c6dd80042c6126d37f42b8c1695
David <david.munoz19@alu.uclm.es>
2025-02-23 09:13:23 +0100

    Formateo de valores null

--- src/main.py
+++ src/main.py
@@ -5,16 +5,26 @@ import pandas as pd
 
 def load(dataset):
     df = pd.read_csv(dataset, header=0, delimiter=",")
+
     # Las fechas que aparecen incompletas las formatea como 01-JAN-YYYY
     df['Incident_Date'] = df['Incident_Date'].apply(lambda x: x.replace('??', '01').replace('???', 'JAN'))
+
     # Elimina las filas duplicadas
     df = df.drop_duplicates()
+
+    # Limpia los datos de la columna "Time" a únicamente la hora
+    df["Time"] = df["Time"].astype(str).str.extract(r'(\d{1,2}:\d{2})')
+
+    # Sutituye los valores nulos por "-"
+    df.fillna("-", inplace=True)
+
+
 
    # print(df.dtypes)
    # print(df["Onboard_Total"]) --> Ex: Fatalities: 0 / Occupants: 7 Transformar en bbdd
    # print(df.isnull().sum())
    # filas_dup = df[df.duplicated()]
-   # print(filas_dup)
+    print(df.isnull().sum())
 
 
 
